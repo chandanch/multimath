ee26c41b4fb57df5438daebdb7f6e38c1d85359d
chandanch <chandan.cec107@gmail.com>
2017-11-24 18:12:53 +0530

    use of type assertion & non-null assertion operator :+1

--- app/app.ts
+++ app/app.ts
@@ -1,17 +1,21 @@
 console.log("Start the typescript game");
 function startGame() {
     // Game will Start
-    var messageElement = document.getElementById('messages');
+    // use of type assertion
+    let messageElement =  (<HTMLElement>document.getElementById('messages'));
     messageElement.innerText = "Game Started!! lets dive in";
 
-    let playerName : string | null | undefined;
-    playerName = null
-    logPlayerName(playerName);
+    let playerName : string;
+    playerName = "Chandio";
+    logPlayerName(playerName); 
 
+    // another example for type assertion
+    let n: number = 5;
+    let s: string = (<number> n).toFixed(4);
 }
 
 function logPlayerName(name)  {
     console.log(`${name} is starting a new game`);
 }
-
-document.getElementById('startGame').addEventListener('click',startGame);
+// use of non null assertion operator
+document.getElementById('startGame')!.addEventListener('click',startGame);
