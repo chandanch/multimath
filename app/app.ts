26b27376409b6432da6413dcdf4539e59dba3494
chandanch <chandan.cec107@gmail.com>
2017-11-25 16:14:12 +0530

    using constructors and parameter properties

--- app/app.ts
+++ app/app.ts
@@ -1,77 +1,21 @@
-console.log("Start the typescript game");
-function startGame() {
-    // Game will Start
-    // use of type assertion
-    let messageElement =  (<HTMLElement>document.getElementById('messages'));
-    messageElement.innerText = "Game Started!! lets dive in";
+/// <reference path="player.ts" />
+/// <reference path="game.ts" />
 
-    let playerName : string;
-    playerName = getPlayerName();
-    postScore(playerName, 300);
-    //postScore(playerName, -3);
-    logMessage(`${playerName}`);
-}
+let newGame: Game;
 
-/**
- * @desc function annotation
- * this function returns string
- */
-function getPlayerName(): string {
-    let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById('playername');
-    return inputElement.value;
-}
+// add click handler to the start game button
+document.getElementById('startGame')!.addEventListener('click', () => {
+    let player: Player = new Player();
+    player.name = Utility.getInputValue('playername');
 
-// use of (optional and default intial value parameter)
-function postScore(playerName?: string, score: number = 0): void {
-    if(playerName == null) {
-        playerName = 'Multimath App'
-    }
-    /* 
-        declare a variable as function type
-        we can assing a function to this variable which takes:
-        string as a parameter and the return type of it is void
-    */
-    let logger: (value: string) => void;
-    if(score < 0) {
-        // assing logger to logError() since logError accepts same parameters
-        // and has the same return type
-        logger = logError;
-    }
-    else {
-        logger = logMessage;
-    }
-    let scoreElement: HTMLElement = (<HTMLElement>document.getElementById('postedScores'));
-    scoreElement.innerText = `${playerName} : ${score}`; 
-    logger(`${score}`); 
-}
+    let problemCount: number = Number(Utility.getInputValue('problemCount'));
+    let factor: number = Number(Utility.getInputValue('factor'));
 
-function logPlayerName(name: string)  {
-    console.log(`${name} is starting a new game`);
-}
-// use of non null assertion operator
-document.getElementById('startGame')!.addEventListener('click',startGame);
+    newGame = new Game(player, problemCount, factor);
+    newGame.displayGame();
+});
 
-/*  Arrow Functions:
-    use of arrow functions to log messages
-    statement to the right of the = of the log message represents the arrow function
-    right of the => represents the function body of the arrow function
-    if the parameters to the arrow function is zero or more then one add the ()
-    if there is only one parameter then no need of (). Exception: if the type of the single parameters
-    is specified use the ()
-*/
-let logMessage = (message: string) => console.log(message);
-let logError = (message: string) => {
-    console.error(message);
-}
-/*  creating an instance of a class
-    To create an instance of a class declare a variable of the class type
-    use the new keyword to call the classes constructor and to create an instance
-    of a class.
-*/
-// creating an instance of Player class - firstPlayer has the copy of the class
-let firstPlayer: Player = new Player();
-// access the class memebers i.e. the property of the class
-firstPlayer.name = 'Chandio';
-firstPlayer.highScore = 34;
-// access the methods of the class
-console.log(firstPlayer.formatName());
+// add click handler to the calculate score button
+document.getElementById('calculate')!.addEventListener('click', () => {
+    newGame.calculateScore();
+});
--- app/player.ts
+++ app/player.ts
@@ -1,13 +1,11 @@
+/// <reference path="person.ts" />
+
 class Player implements Person {
     name: string;
     age: number;
     highScore: number;
-
-    formatName(): string {
-        /*  use the `this` keyword to refer to the memebers of the class.
-            this keyword followed by the period(.) and the 
-            member of the class that we want to reference 
-        */
+    
+    formatName() {
         return this.name.toUpperCase();
     }
 }
